16550c24ea7b5626317759011250dc25acb0d65f
A.J. Norton <ajn123@vt.edu>
2014-10-14 20:35:07 -0400

    push to heroku

--- config/application.rb
+++ config/application.rb
@@ -20,6 +20,8 @@ module BootstrapBlog
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
 
+    config.assets.paths << Rails.root.join('vendor', 'assets', 'components')
+
     config.assets.precompile += %w( .svg .eot .woff .ttf )
   end
 end
