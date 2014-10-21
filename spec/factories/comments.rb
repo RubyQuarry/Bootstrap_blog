68e4167a656c2fd87bdfb5d961a752dd4650d46f
A.J. Norton <ajn123@vt.edu>
2014-10-21 03:45:17 -0400

    updated readme

---
+++ spec/factories/comments.rb
@@ -0,0 +1,11 @@
+require 'faker'
+
+
+
+FactoryGirl.define do
+  factory :comment do |f|
+    f.name "aj"
+    f.text "this is it"
+
+  end
+end
--- spec/models/blog_spec.rb
+++ spec/models/blog_spec.rb
@@ -2,8 +2,6 @@ require 'rails_helper'
 
 RSpec.describe Blog, :type => :model do
 
-
-
   it "has a title" do
     expect(FactoryGirl.create(:blog)).to be_valid
     expect(FactoryGirl.build(:blog_error)).to_not be_valid
